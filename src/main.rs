1d40d4a839f282851327753226de6b24b21a00d0
Malte Kauranen <malte.kauranen@gmail.com>
2021-12-20 10:31:53 +0100

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 mod game;
 mod chess;
+pub mod ai;
 
 use std::io::Write;
 use std::io::stdout;
 use std::io::stdin;
 
-use crate::chess::chess_move;
 use crate::game::game::Game;
 use crate::chess::chess_move::ChessMove;
 
@@ -21,10 +21,14 @@ fn main() {
         let parsed_chess_move = ChessMove::from(chess_move_string);
         match parsed_chess_move {
             None => println!("Incorrect move"),
-            Some(chess_move) => if game.legal_move(&chess_move) {
-                game.make_move(&chess_move);
-            }
+            Some(chess_move) => make_move(&mut game, &chess_move)
         }
         print!("{}", game);
+    }
+}
+
+fn make_move(game: &mut Game, chess_move: &ChessMove) {
+    if game.legal_move(chess_move) {
+        game.make_move(chess_move);
     }
 }
