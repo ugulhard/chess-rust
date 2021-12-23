08a2e4e0d6f929b7876d4f9ce1c6a1f4d6cdc70a
Malte Kauranen <malte.kauranen@gmail.com>
2021-12-23 09:09:46 +0100

    actually bench ai

--- benches/benchmark_alpha_beta_search.rs
+++ benches/benchmark_alpha_beta_search.rs
@@ -1,3 +1,4 @@
+use chess_rust::ai::ai::Ai;
 use criterion::{criterion_group, criterion_main, Criterion};
 use chess_rust::chess::board::Board;
 use chess_rust::chess::color::Color;
@@ -5,10 +6,8 @@ use chess_rust::ai::alpha_beta_ai::AlphaBetaAi;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let board = Board::new();
-    let alpha =&mut f64::MIN.clone();
-    let beta =&mut f64::MAX.clone();
-    let ai = &mut AlphaBetaAi::new(Color::White, 7);
-    c.bench_function("Move generation", |b| b.iter(|| board.legal_moves()));
+    let ai = &mut AlphaBetaAi::new(Color::White, 5);
+    c.bench_function("Move generation", |b| b.iter(|| ai.find_best_move(&board)));
 }
 
 criterion_group!(benches, criterion_benchmark);
